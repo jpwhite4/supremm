6ba9550cb8af0e644c397bcdb7ac7cca38dc97f8
Joseph White <jpwhite4@buffalo.edu>
2017-03-28 15:54:40 -0400

    Fix pylint errors.

--- supremm/proc_common.py
+++ supremm/proc_common.py
@@ -84,7 +84,7 @@ def getoptions(has_mpi):
     opts, _ = getopt(sys.argv[1:], "ABONCbP:M:j:r:t:dqs:e:LT:t:D:Eo:h",
                      ["localjobid=",
                       "resource=",
-                      "threads=", 
+                      "threads=",
                       "debug",
                       "quiet",
                       "start=",
@@ -204,49 +204,49 @@ def summarizejob(job, conf, resconf, plugins, preprocs, m, dblog, opts):
         mdata = {}
         mergestart = time.time()
 
-        summarizeerror=None
+        summarizeerror = None
 
         if job.nodecount > 1 and job.walltime < 5 * 60:
             mergeresult = 1
             mdata["skipped_parallel_too_short"] = True
-            summarizeerror=ProcessingError.PARALLEL_TOO_SHORT
+            summarizeerror = ProcessingError.PARALLEL_TOO_SHORT
             # Was "skipped"
             missingnodes = job.nodecount
             logging.info("Skipping %s, skipped_parallel_too_short", job.job_id)
         elif job.walltime <= 180:
             mergeresult = 1
             mdata["skipped_too_short"] = True
-            summarizeerror=ProcessingError.TIME_TOO_SHORT
+            summarizeerror = ProcessingError.TIME_TOO_SHORT
             missingnodes = job.nodecount
             logging.info("Skipping %s, skipped_too_short", job.job_id)
         elif job.nodecount < 1:
             mergeresult = 1
             mdata["skipped_invalid_nodecount"] = True
-            summarizeerror=ProcessingError.INVALID_NODECOUNT
+            summarizeerror = ProcessingError.INVALID_NODECOUNT
             missingnodes = job.nodecount
             logging.info("Skipping %s, skipped_invalid_nodecount", job.job_id)
         elif not job.has_any_archives():
             mergeresult = 1
             mdata["skipped_noarchives"] = True
-            summarizeerror=ProcessingError.NO_ARCHIVES
+            summarizeerror = ProcessingError.NO_ARCHIVES
             missingnodes = job.nodecount
             logging.info("Skipping %s, skipped_noarchives", job.job_id)
         elif not job.has_enough_raw_archives():
             mergeresult = 1
             mdata["skipped_rawarchives"] = True
-            summarizeerror=ProcessingError.RAW_ARCHIVES
+            summarizeerror = ProcessingError.RAW_ARCHIVES
             missingnodes = job.nodecount
             logging.info("Skipping %s, skipped_rawarchives", job.job_id)
         elif opts['max_nodes'] > 0 and job.nodecount > opts['max_nodes']:
             mergeresult = 1
             mdata["skipped_job_too_big"] = True
-            summarizeerror=ProcessingError.JOB_TOO_BIG
+            summarizeerror = ProcessingError.JOB_TOO_BIG
             missingnodes = job.nodecount
             logging.info("Skipping %s, skipped_job_too_big", job.job_id)
         elif job.walltime >= 176400:
             mergeresult = 1
             mdata["skipped_too_long"] = True
-            summarizeerror=ProcessingError.TIME_TOO_LONG
+            summarizeerror = ProcessingError.TIME_TOO_LONG
             missingnodes = job.nodecount
             logging.info("Skipping %s, skipped_too_long", job.job_id)
         else:
@@ -254,17 +254,17 @@ def summarizejob(job, conf, resconf, plugins, preprocs, m, dblog, opts):
             missingnodes = -1.0 * mergeresult
         mergeend = time.time()
 
-        if opts['extractonly']: 
+        if opts['extractonly']:
             return 0 == mergeresult
 
         preprocessors = [x(job) for x in preprocs]
         analytics = [x(job) for x in plugins]
         s = Summarize(preprocessors, analytics, job, conf)
 
-        enough_nodes=False
+        enough_nodes = False
 
-        if 0 == mergeresult or ( job.nodecount != 0 and (missingnodes / job.nodecount < 0.05)):
-            enough_nodes=True
+        if 0 == mergeresult or (job.nodecount != 0 and (missingnodes / job.nodecount < 0.05)):
+            enough_nodes = True
             logging.info("Success for %s files in %s (%s/%s)", job.job_id, job.jobdir, missingnodes, job.nodecount)
             s.process()
         elif summarizeerror == None and job.nodecount != 0 and (missingnodes / job.nodecount >= 0.05):
@@ -272,10 +272,10 @@ def summarizejob(job, conf, resconf, plugins, preprocs, m, dblog, opts):
             # Don't have enough node data to even try summarization
             mdata["skipped_pmlogextract_error"] = True
             logging.info("Skipping %s, skipped_pmlogextract_error", job.job_id)
-            summarizeerror=ProcessingError.PMLOGEXTRACT_ERROR
+            summarizeerror = ProcessingError.PMLOGEXTRACT_ERROR
 
         mdata["mergetime"] = mergeend - mergestart
-        
+
         if opts['tag'] != None:
             mdata['tag'] = opts['tag']
 
@@ -291,7 +291,7 @@ def summarizejob(job, conf, resconf, plugins, preprocs, m, dblog, opts):
             # All other "known" errors should already be handled above.
             mdata["skipped_summarization_error"] = True
             logging.info("Skipping %s, skipped_summarization_error", job.job_id)
-            summarizeerror=ProcessingError.SUMMARIZATION_ERROR
+            summarizeerror = ProcessingError.SUMMARIZATION_ERROR
 
         force_success = False
         if not success:
@@ -311,7 +311,7 @@ def summarizejob(job, conf, resconf, plugins, preprocs, m, dblog, opts):
     return success
 
 def override_defaults(resconf, opts):
-    """ Commandline options that override the configruation file settings """
+    """ Commandline options that override the configuration file settings """
     if 'job_output_dir' in opts and opts['job_output_dir'] != None:
         resconf['job_output_dir'] = opts['job_output_dir']
 
@@ -321,14 +321,14 @@ def filter_plugins(resconf, preprocs, plugins):
     """ Filter the list of plugins/preprocs to use on a resource basis """
 
     # Default is to use all
-    filtered_preprocs=preprocs
-    filtered_plugins=plugins
+    filtered_preprocs = preprocs
+    filtered_plugins = plugins
 
     if "plugin_whitelist" in resconf:
-       filtered_preprocs = [x for x in preprocs if x.__name__ in resconf['plugin_whitelist']]
-       filtered_plugins = [x for x in plugins if x.__name__ in resconf['plugin_whitelist']]
+        filtered_preprocs = [x for x in preprocs if x.__name__ in resconf['plugin_whitelist']]
+        filtered_plugins = [x for x in plugins if x.__name__ in resconf['plugin_whitelist']]
     elif "plugin_blacklist" in resconf:
-       filtered_preprocs = [x for x in preprocs if x.__name__ not in resconf['plugin_blacklist']]
-       filtered_plugins = [x for x in plugins if x.__name__ not in resconf['plugin_blacklist']]
+        filtered_preprocs = [x for x in preprocs if x.__name__ not in resconf['plugin_blacklist']]
+        filtered_plugins = [x for x in plugins if x.__name__ not in resconf['plugin_blacklist']]
 
     return filtered_preprocs, filtered_plugins
